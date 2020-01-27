1148c1e9ba0f71fc4599efde42ed24dbdd450665
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-27 12:35:42 +0000

    test: create Deposit class to seperate deposits

---
+++ spec/deposit_spec.rb
@@ -0,0 +1,13 @@
+require 'deposit'
+
+RSpec.describe Deposit do
+  let(:test_deposit) { Deposit.new(500, '10/01/2020') }
+
+  it 'takes an amount' do
+    expect(test_deposit.view_amount).to be 500
+  end
+
+  it 'takes a date' do
+    expect(test_deposit.date_created).to eq '10/01/2020'
+  end
+end
