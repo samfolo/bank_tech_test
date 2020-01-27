d0dadd0bbec700a47d3c68c310cd0c154ee71d61
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-27 12:06:11 +0000

    feat: owner can view their current balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,19 @@
 class BankAccount
+  def initialize balance = 0
+    @balance = balance
+  end
+
   def deposit amount
+    @balance += amount
     "You have deposited #{amount} coins"
   end
 
   def withdraw amount
+    @balance -= amount
     "You have withdrawn #{amount} coins"
+  end
+
+  def view_balance
+    "You have #{@balance} coins"
   end
 end
--- spec/features/working_account_spec.rb
+++ spec/features/working_account_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe 'a working bank account', type: :feature do
     test_account.deposit(500)
     expect(test_account.withdraw(250)).to eq 'You have withdrawn 250 coins'
   end
+
+  scenario 'a worker makes a deposit of 500, withdraws 30 and wants to see their balance' do
+    test_account.deposit(500)
+    test_account.withdraw(30)
+    expect(test_account.view_balance).to eq 'You have 470 coins'
+  end
 end
