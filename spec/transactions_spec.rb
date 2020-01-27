a691f2d80974bedaf8c67a3e5a1d68f687259666
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-27 15:34:05 +0000

    feat: transaction class instance can deal with a mix of deposits and withdrawals

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe Transactions do
   let(:withdrawal_two) { double(:withdrawal, log_line: '12/12/2020 || 50.00 || || 410.00') }
   let(:withdrawal_three) { double(:withdrawal, log_line: '24/04/2022 || 10.00 || || 400.00') }
 
-  before do
-    
-  end
-
   it 'can return data for three seperate deposits' do
     test_transctions.log(deposit_one)
     test_transctions.log(deposit_two)
@@ -30,6 +26,15 @@ RSpec.describe Transactions do
     test_transctions.log(withdrawal_three)
 
     expected_log = ['11/12/2020 || 40.00 || || 460.00', '12/12/2020 || 50.00 || || 410.00', '24/04/2022 || 10.00 || || 400.00']
+    expect(test_transctions.log_data).to eq expected_log
+  end
+
+  it 'can return data for two seperate withdrawals and one deposit' do
+    test_transctions.log(deposit_one)
+    test_transctions.log(withdrawal_one)
+    test_transctions.log(withdrawal_two)
+
+    expected_log = ['10/12/2020 || 500.00 || || 500.00', '11/12/2020 || 40.00 || || 460.00', '12/12/2020 || 50.00 || || 410.00']
     expect(test_transctions.log_data).to eq expected_log
   end
 end
